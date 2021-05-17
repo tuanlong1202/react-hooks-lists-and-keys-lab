66f726efabf91872f2f6c63feee73aefea43a9fd
tuanlong1202 <tuanlong1202@gmail.com>
2021-05-16 23:52:57 -0400

    React hooks lists and keys lab Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const pre = "#";
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <a href={pre+links[0]}>Home</a>
+      <a href={pre+links[1]}>About</a>
+      <a href={pre+links[2]}>Projects</a>
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologyList = technologies.map((value, key) =>
+    <span id={key}>{value}</span>
+  );
   return (
-    <div className="project-item">
+    <div className="project-item"> 
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
+        {technologyList}
         {/* render a <span> for each technology in the technologies array */}
       </div>
     </div>
